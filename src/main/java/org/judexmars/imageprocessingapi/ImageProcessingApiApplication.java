f9a09a123305d1c80ea7d54c12bf87b29599e453
JudexMars <danielpro2013rus@gmail.com>
2024-03-26 00:45:11 +0300

    init: basic initialization

---
+++ src/main/java/org/judexmars/imageprocessingapi/ImageProcessingApiApplication.java
@@ -0,0 +1,13 @@
+package org.judexmars.imageprocessingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingApiApplication.class, args);
+	}
+
+}
